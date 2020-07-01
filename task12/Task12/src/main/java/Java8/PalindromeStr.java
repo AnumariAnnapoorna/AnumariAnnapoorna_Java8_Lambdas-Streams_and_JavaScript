e1a01c77dfc2c448f197ba55dc84dc4933e902e4
AnumariAnnapoorna <60225066+AnumariAnnapoorna@users.noreply.github.com>
2020-07-01 13:48:14 -0400

    Commit

--- task12/Task12/src/main/java/Java8/PalindromeStr.java
+++ task12/Task12/src/main/java/Java8/PalindromeStr.java
@@ -3,8 +3,6 @@ package Java8;
 import java.util.ArrayList;
 import java.util.function.*;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class PalindromeStr {
 

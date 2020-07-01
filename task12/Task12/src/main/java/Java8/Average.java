cfd27fd282a227217e0d519e8766e09e23da56e8
AnumariAnnapoorna <60225066+AnumariAnnapoorna@users.noreply.github.com>
2020-07-01 13:50:45 -0400

    Commit

--- task12/Task12/src/main/java/Java8/Average.java
+++ task12/Task12/src/main/java/Java8/Average.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.IntSummaryStatistics;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Average {
 	public static void main(String args[]) throws IOException
